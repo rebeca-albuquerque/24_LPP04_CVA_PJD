4990e83063443b5d62bad9e4f5cf00b94dbc9d64
rebeca-albuquerque <149831465+rebeca-albuquerque@users.noreply.github.com>
2024-09-10 12:56:00 +0100

    Add files via upload

--- velocidade.py
+++ velocidade.py
@@ -9,4 +9,20 @@ if velocidade > 80:
     print(f"Valor da multa: €{multa:.2f}")
 else:
     print("Velocidade dentro do limite permitido. Não há multa.")
-#um comentário para alteração.    
+    
+    
+#um comentário para alteração.   
+ 
+''' git diff: Mostra as diferenças entre arquivos alterados mas ainda não "stageados" ou entre commits.
+git commit: Salva as mudanças do staged area no repositório local com uma mensagem explicando as alterações.
+git merge: Combina alterações de uma branch com outra.
+git log: Exibe o histórico de commits.
+Outros comandos úteis:
+
+git add: Adiciona alterações ao staged area para serem commitadas.
+git checkout: Muda de branch ou restaura arquivos.
+git branch: Gerencia branches (criar, listar, deletar).
+git push: Envia commits do repositório local para o remoto.
+git pull: Atualiza o repositório local com mudanças do repositório remoto.
+git clone: Clona um repositório remoto para o local.
+Esses comandos são essenciais para o fluxo de trabalho no Git.'''
