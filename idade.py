8be0f0500212cafe76ea37e666107d1579819802
rebeca-albuquerque <149831465+rebeca-albuquerque@users.noreply.github.com>
2024-09-10 12:54:15 +0100

    Add files via upload

--- idade.py
+++ idade.py
@@ -12,3 +12,17 @@ elif idade >= 18:
     print("És um adulto")
 else:
     print("És uma criança")
+
+''' git diff: Mostra as diferenças entre arquivos alterados mas ainda não "stageados" ou entre commits.
+git commit: Salva as mudanças do staged area no repositório local com uma mensagem explicando as alterações.
+git merge: Combina alterações de uma branch com outra.
+git log: Exibe o histórico de commits.
+Outros comandos úteis:
+
+git add: Adiciona alterações ao staged area para serem commitadas.
+git checkout: Muda de branch ou restaura arquivos.
+git branch: Gerencia branches (criar, listar, deletar).
+git push: Envia commits do repositório local para o remoto.
+git pull: Atualiza o repositório local com mudanças do repositório remoto.
+git clone: Clona um repositório remoto para o local.
+Esses comandos são essenciais para o fluxo de trabalho no Git.'''
